8a828a6ebef84245afb82fcff223df6d987fda4e
Jeremy Satram <50312818+Jsatram@users.noreply.github.com>
2023-04-11 17:07:27 -0600

    Update project3.lisp
    added skeleton for functions 3 and 4

--- project3.lisp
+++ project3.lisp
@@ -8,3 +8,13 @@
 ; Testing with the examples provided
 (print(set-member '(1 2) 1))
 (print(set-member '(1 2) 3))
+
+; Part 3 Implementation
+(defun set-intersection (set-1 set-2)
+      ;Your implementation go here
+      )
+
+; part 4 Implementation
+(defun set-diff (set-1 set-2)
+      ;Your implementation go here
+      )
